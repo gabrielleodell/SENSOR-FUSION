20326e93171c1a55ffaa9e5be181482578558771
Gabby O'Dell Vanner <grodell@wpi.edu>
2022-12-14 18:01:34 -0500

    Cleaned up code

--- sensor_fusion/model_states/boxes.py
+++ sensor_fusion/model_states/boxes.py
@@ -6,47 +6,44 @@ from std_msgs.msg import Header
 from gazebo_msgs.srv import GetModelState, GetModelStateRequest
 
 rospy.init_node('odom_pub', anonymous=True)
-
-odom_pub_x=rospy.Publisher('/box_x', Odometry, queue_size=10)
-odom_pub_y=rospy.Publisher('/box_y', Odometry, queue_size=10)
+r = rospy.Rate(2)
 
 rospy.wait_for_service('/gazebo/get_model_state')
 get_model_srv=rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
 
+# Setup x
+odom_pub_x=rospy.Publisher('/box_x', Odometry, queue_size=10)
 odom_x=Odometry()
-odom_y=Odometry()
-
 header_x = Header()
-header_y = Header()
-
 header_x.frame_id='/odom'
-header_y.frame_id='/odom'
-
 model_x = GetModelStateRequest()
-model_y = GetModelStateRequest()
-
 model_x.model_name='Box_1'
+
+# Set up y
+odom_pub_y=rospy.Publisher('/box_y', Odometry, queue_size=10)
+odom_y=Odometry()
+header_y = Header()
+header_y.frame_id='/odom'
+model_y = GetModelStateRequest()
 model_y.model_name='Box_2'
 
-r = rospy.Rate(2)
 
 while not rospy.is_shutdown():
+
+    # Run x
     result_x = get_model_srv(model_x)
-    result_y = get_model_srv(model_y)
-
     odom_x.pose.pose = result_x.pose
-    odom_y.pose.pose = result_y.pose
-
     odom_x.twist.twist = result_x.twist
-    odom_y.twist.twist = result_y.twist
-
     header_x.stamp = rospy.Time.now()
-    header_y.stamp = rospy.Time.now()
-
     odom_x.header = header_x
-    odom_y.header = header_y
-
     odom_pub_x.publish(odom_x)
+
+    # Run y
+    result_y = get_model_srv(model_y)
+    odom_y.pose.pose = result_y.pose
+    odom_y.twist.twist = result_y.twist
+    header_y.stamp = rospy.Time.now()
+    odom_y.header = header_y
     odom_pub_y.publish(odom_y)
 
     r.sleep()
